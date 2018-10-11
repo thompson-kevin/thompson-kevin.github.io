75f04624c8d3be6c82e46f216e2260f74eb18b75
Kevin Thompson <vangpao@hotmail.com>
2018-10-10 21:13:03 -0700

    Fixed after key started working

--- javascript/json.js
+++ javascript/json.js
@@ -6,12 +6,21 @@ function getWeather() {
     if (zipCode.length === 5) {
         var oReq = new XMLHttpRequest();
         var url = 'https://api.openweathermap.org/data/2.5/weather?zip={zipCode},us&APPID=3b7c61b3e3937e09f648d488b3aebcfa';
-        url.replace('{zipCode}', zipCode);
+        url = url.replace('{zipCode}', zipCode);
         oReq.open('GET', url);
+        oReq.outputBox = outputBox;
+        oReq.rawOutputBox = rawOutputBox;
+        oReq.printResponse = function(message, output) {
+            output.innerHTML = message;
+        };
+        oReq.onreadystatechange = function() {
+            if (oReq.readyState === 4) {
+                var response = JSON.parse(oReq.response);
+                oReq.printResponse(JSON.stringify(response), oReq.rawOutputBox);
+                oReq.printResponse(response.weather[0].description, oReq.outputBox);
+            }
+          }
         oReq.send();
-        var response = JSON.parse(oReq);
-        this.printResponse(JSON.stringify(response), rawOutputBox);
-        this.printResponse(response.weather.description, outputBox);
     } else {
         this.printWarning('That is not a valid zip code. Please input a 5 digit Zip', outputBox);
     }
